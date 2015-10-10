1d5072b3fb6b4f58d4e5c724ef4487456cffc5bc
Eric Ettlin <mrumrocks@gmail.com>
2015-10-09 22:07:08 -0700

    bullets not ending

--- src/gamesrc/BulletOne.java
+++ src/gamesrc/BulletOne.java
@@ -2,12 +2,46 @@ package gamesrc;
 
 import java.awt.Image;
 import java.awt.Point;
+import java.util.List;
+
+import jgame.Context;
+import jgame.GObject;
+import jgame.listener.FrameListener;
 
 public class BulletOne extends Bullet {
+	private Point startPosition = null;
+	private Point currentPosition = null;
+
 	public BulletOne(Image image, Point p) {
 		super(image);
+		startPosition = p;
+		currentPosition = p;
+		this.addListener(new FrameListener() {
+			public void invoke(GObject target, Context context) {
+              if(getDistanceFromStart() > 200){
+            	  target.removeSelf(); //++++++++++++++++++++++++  Causing Crash
+              }
+			}
+		});
+		
 
 	}
-	
-	
+
+	public Point getStartPosition() {
+		return startPosition;
+	}
+
+	public Point getCurrentPosition() {
+		currentPosition.setLocation((int) this.getX(), (int) this.getY());
+		return currentPosition;
+	}
+
+	public double getDistanceFromStart() {
+		double dx = startPosition.getX() - getCurrentPosition().getX();
+		double dy = startPosition.getY() - getCurrentPosition().getY();
+		
+	    System.out.println(" spx = " + startPosition.getX() + ",  spy = " + startPosition.getY());
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+
 }
